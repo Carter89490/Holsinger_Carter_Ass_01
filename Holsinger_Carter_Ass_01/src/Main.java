7e527a1ca6659ac94c11e35c07f49bf06d0f5048
Carter Holsinger <113468540+Carter89490@users.noreply.github.com>
2022-10-09 22:09:07 -0400

    Add files via upload

---
+++ Holsinger_Carter_Ass_01/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main
+{
+    public static void main(String[] args) {
+        final int ROCK = 1, SCISSOR = 2, PAPER = 3;
+        {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Player 1: Choose (1) - Rock, (2) - Scissors, or (3) - Paper: ");
+            int player1 = scan.nextInt();
+            System.out.println("Player 2: Choose (1) - Rock, (2) - Scissors, or (3) - Paper: ");
+            int player2 = scan.nextInt();
+
+            if (player1 == player2) {
+                System.out.print("It is a tie");
+            } else {
+                switch (player1) {
+                    case ROCK:
+                        if (player2 == SCISSOR)
+                            System.out.print("Player 1 wins!");
+                        else
+                            System.out.print("Player 2 wins!");
+                        break;
+                    case SCISSOR:
+                        if (player2 == PAPER)
+                            System.out.print("Player 1 wins!");
+                        else
+                            System.out.print("Player 2 wins!");
+                        break;
+                    case PAPER:
+                        if (player2 == ROCK)
+                            System.out.print("Player 1 wins!");
+                        else
+                            System.out.print("Player 2 wins!");
+                        break;
+                }
+            }
+        }
+    }
+
+
+
