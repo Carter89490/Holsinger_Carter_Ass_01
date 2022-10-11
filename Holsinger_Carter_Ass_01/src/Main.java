88a521c2a4ccb88b4232fb4da34f20b7fa871772
Carter Holsinger <113468540+Carter89490@users.noreply.github.com>
2022-10-11 13:58:56 -0400

    Add files via upload

--- Holsinger_Carter_Ass_01/src/Main.java
+++ Holsinger_Carter_Ass_01/src/Main.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 import static java.lang.System.in;
-public class Main
-{
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(in);
-            System.out.println("Player 1: Choose rock, scissors, or paper:");
-            String player1 = scan.next().toLowerCase();
-            System.out.println("Player 2: Choose rock, scissors, or paper:");
-            String player2 = scan.next().toLowerCase();
-            boolean done = true;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(in);
+        System.out.println("Player 1: Choose rock, scissors, or paper:");
+        String player1 = scan.next().toLowerCase();
+        System.out.println("Player 2: Choose rock, scissors, or paper:");
+        String player2 = scan.next().toLowerCase();
+        boolean done = true;
+
             do {
                 if (player1.equals(player2)) {
                     System.out.print("It is a tie");
-                } else if (player1.equals("P") && player2.equals("R")) {
+                } else if (player1.equals("p") && player2.equals("r")) {
                     System.out.println("Player one wins");
-                } else if (player1.equals("S") && player2.equals("R")) {
+                } else if (player1.equals("s") && player2.equals("r")) {
                     System.out.println("Player two wins");
-                } else if (player1.equals("R") && player2.equals("P")) {
+                } else if (player1.equals("r") && player2.equals("p")) {
                     System.out.println("Player two wins");
-                } else if (player1.equals("R") && player2.equals("S")) {
+                } else if (player1.equals("r") && player2.equals("s")) {
                     System.out.println("Player one wins");
                 }
-                done = false;
-            } while(done);
+                System.out.println("do you wish to play again True, False: ");
+                if (scan.hasNext()) {
+                    done = scan.nextBoolean();
+                }
+            } while (done);
         }
-}
+    }
+
 
 
 
